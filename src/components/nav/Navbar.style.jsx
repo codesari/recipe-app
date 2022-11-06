24c07a278f8727e2a13fbc9e7d7ebf609fce29d5
codesari <ahmtsr81@gmail.com>
2022-11-06 17:05:48 +0300

    dynamic style

---
+++ src/components/nav/Navbar.style.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import Flex from "../styled/Flex.Styled";
+
+export const NavbarStyled = styled(Flex)`
+  padding: 1rem;
+  /* justify-content: space-between; */
+  //? justify'ı burada degistirmek yerine component icinde yazarak dynamic olarak style veriyoruz
+`;
