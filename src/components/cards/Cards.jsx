b8dad5c4144d0cc7ed23a4d075cf92253a3d7608
codesari <ahmtsr81@gmail.com>
2022-11-09 19:51:48 +0300

    replace false

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Button, Card, Header, Image, MainContainer } from "./Cards.style";
+import defaultImage from "../../assets/default-image.jpg";
+import { useNavigate } from "react-router-dom";
 
 const Cards = ({ recipes }) => {
-  return <div>Cards</div>;
+  const navigate = useNavigate();
+  return (
+    <MainContainer wrap="wrap">
+      {recipes.map(({ recipe }, index) => {
+        return (
+          <Card key={index}>
+            <Header>{recipe.label} </Header>
+            <Image src={recipe.image || defaultImage} />
+            <Button
+              onClick={
+                () => navigate("detail", { state: recipe, replace: false })
+                // navigate("detail", { state: recipe, replace: false })
+                //! replace:false old. takdirde history bilgisi tutulmaz.navigate ile -1 diyemeyiz
+              }
+            >
+              View More
+            </Button>
+          </Card>
+        );
+      })}
+    </MainContainer>
+  );
 };
 
 export default Cards;
